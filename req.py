4f22786da575a803a92bad72edefa79695a6dd87
eyqs <eugeneyqshen@gmail.com>
2016-04-09 15:24:09 -0700

    Read course list input into course objects

--- req.py
+++ req.py
@@ -1,30 +1,65 @@
 #!/usr/bin/env python
 DATAFILE = 'cpsc.txt'
 
-# Parse DATAFILE to find the courses
+# Generic class to store course data
+class Course():
+    def __init__(self, code):
+        self.code = code
+        self.name = ''
+        self.cred = []
+        self.preq = []
+        self.creq = []
+        self.term = []
+        self.excl = []
+
+    # Set course parameters
+    def set_params(self, param, value):
+        if param == 'name':
+            self.name = value
+        elif param == 'cred':
+            self.cred = [int(c.strip()) for c in value.split(',')]
+        elif param == 'preq':
+            self.preq = self.get_reqs(value)
+        elif param == 'creq':
+            self.creq = self.get_reqs(value)
+        elif param == 'term':
+            self.term = [t.strip() for t in value.split(',')]
+        elif param == 'excl':
+            self.excl = [e.strip() for e in value.split(',')]
+        else:
+            print('Error: parameter not recognized.')
+
+    # Get course parameters
+    def get_params(self, param=''):
+        params = {'name':self.name, 'cred':self.cred, 'preq':self.preq,
+                  'creq':self.creq, 'term':self.term, 'excl':self.excl}
+        if param in params.keys():
+            return params[param]
+        else:
+            return params
+
+    # Turn requisites into list format; all entries must be true to satisfy
+    def get_reqs(self, value):
+        pass
+
+
+# Parse DATAFILE to create the course graph
 def parse():
     courses = {}
     with open(DATAFILE) as f:
         for line in f:
             split = line.split(':')
-            param = split[0].strip()
-            if param == 'code':
-                curr = {'name':'', 'cred':'', 'preq':'',
-                        'creq':'', 'term':'', 'excl':''}
-                courses[split[1].strip()] = curr
-            elif len(split) > 1:
+            if len(split) > 1:
+                param = split[0].strip()
                 value = split[1].strip()
-                if param == 'name':
-                    curr[param] = value
-                elif param == 'cred':
-                    curr[param] = [int(c) for c in value.split(',')]
-                elif param == 'preq' or param == 'creq':
-                    pass
-                elif param == 'term' or param == 'excl':
-                    curr[param] = [t.strip() for t in value.split(',')]
+                if param == 'code':
+                    course = Course(value)
+                    courses[value] = course
+                else:
+                    course.set_params(param, value)
     return courses
 
+
 if __name__ == '__main__':
-    # Parse the course list
     courses = parse()
-    print(courses['CPSC 110'])
+    print(courses['CPSC 110'].get_params())
