e2880d668781f1e233ff0fb6eef348bb6a65f488
eyqs <eugeneyqshen@gmail.com>
2016-04-13 00:40:24 -0700

    Try to parse reqs from html and fail horribly

--- web.py
+++ web.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 """
-req v1.0.2
+req v1.0.4
 Copyright © 2016 Eugene Y. Q. Shen.
 
 req is free software: you can redistribute it and/or
@@ -61,12 +61,67 @@ def translate(name):
                 creq = ''
                 for i in range(len(split)):
                     if 'em>Prerequisite:' in split[i]:
-                        preq = split[i+1][5:]
+                        preq = parse(split[i+1][5:-1])
                     elif 'em>Corequisite:' in split[i]:
-                        creq = split[i+1][5:]
+                        creq = parse(split[i+1][5:-1])
                 outfile.write('\ndesc: ' + desc)
                 outfile.write('\npreq: ' + preq)
                 outfile.write('\ncreq: ' + creq)
+
+def parse(reqs):
+    left = reqs.split()
+    parsed = []         # Stores the parsed requirements
+    clause = []         # Stores the courses in a clause
+    course = []         # Stores the name of a course
+    hasand = False      # Clause has 'and'
+    hasors = False      # Clause has 'or'
+    i = 0
+    while i < len(left):
+        doesnt = False  # No operator reached, still part of a name
+        change = False  # Operator type changed
+        if left[i].lower() == 'and':    # 'and' between ands
+            if not hasand:
+                hasand = True
+                hasors = False
+        elif left[i].lower() == 'one':  # 'one of' begins ors
+            if not hasors:
+                hasors = True
+                hasand = False
+                change = True
+            i += 1
+        elif left[i] == 'or':           # 'or' between ors
+            if not hasors:
+                hasors = True
+                hasand = False
+        else:
+            doesnt = True
+            course.append(left[i])      # no operator, part of course name
+        if not doesnt and len(course) > 0:
+            clause.append(' '.join(course))
+            course = []
+        if change and len(clause) > 0:  # operator type changed, add clause
+            if hasand:
+                parsed.append('(' + ' and '.join(clause) + ')')
+                clause = []
+            if hasors:
+                parsed.append('(' + ' or '.join(clause) + ')')
+                clause = []
+        i += 1
+    clause.append(' '.join(course))     # append final courses and clauses
+    if hasand:
+        parsed.append('(' + ' and '.join(clause) + ')')
+    elif hasors:
+        parsed.append('(' + ' or '.join(clause) + ')')
+    else:
+        parsed = clause
+    return(' '.join(flatten(parsed)))
+
+def flatten(lislis):
+    for lis in lislis:
+        if isinstance(lis, list) and not isinstance(lis, str):
+            yield from flatten(lis)
+        else:
+            yield lis
 
 if __name__ == '__main__':
     for filename in os.listdir(FOLDER):
