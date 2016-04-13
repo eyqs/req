37fb14a1ee6e06e3ec57cd049d8039bc1db0e59d
eyqs <eugeneyqshen@gmail.com>
2016-04-12 21:22:43 -0700

    Get all info from html and ignore grad courses

--- web.py
+++ web.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 """
-req v1.0.1
+req v1.0.2
 Copyright © 2016 Eugene Y. Q. Shen.
 
 req is free software: you can redistribute it and/or
@@ -21,19 +21,26 @@ FOLDER  = 'courses' # relative path to folder with course lists
 
 def translate(name):
     with open(name + '.html') as infile:
-        # Skip all lines before the actual course table
+        # Skip all lines before the actual course list
         for line in infile:
             if line.strip() != '<dl class="double">':
                 continue
             break
         # Open the output .txt file and start writing to it
         outfile = open(name + '.txt', 'w')
+        hascode = False
         for line in infile:
+            if line.strip() == '</dl>': # End of course list
+                break
             # ['\t', 'dt>', 'a name="121">', '/a>CPSC 121 (4)  ',
             #  'b>Models of Computation', '/b>', '/dt>\n']
             if line.strip().startswith('<dt>'):
+                hascode = True
                 split = line.split('<')
                 code = ' '.join(split[3][3:].split()[:2])
+                if int(code.split()[1]) >= 500: # Skip grad courses
+                    hascode = False
+                    continue
                 name = split[4][2:]
                 cred = split[3].split('(')[1].split(')')[0]
                 if '-' in cred:                 # Put credits into a range
@@ -44,6 +51,22 @@ def translate(name):
                 outfile.write('\n\ncode: ' + code)
                 outfile.write('\nname: ' + name)
                 outfile.write('\ncred: ' + cred)
+            # ['\t', 'dd>Structures of computation. [3-2-1]', 'br > ',
+            #  'em>Prerequisite:', '/em> Principles of Mathematics 12',
+            #  'br> ', 'em>Corequisite:', '/em> CPSC 110.', 'br> \n']
+            elif hascode and line.strip().startswith('<dd>'):
+                split = line.split('<')
+                desc = split[1][3:]
+                preq = ''
+                creq = ''
+                for i in range(len(split)):
+                    if 'em>Prerequisite:' in split[i]:
+                        preq = split[i+1][5:]
+                    elif 'em>Corequisite:' in split[i]:
+                        creq = split[i+1][5:]
+                outfile.write('\ndesc: ' + desc)
+                outfile.write('\npreq: ' + preq)
+                outfile.write('\ncreq: ' + creq)
 
 if __name__ == '__main__':
     for filename in os.listdir(FOLDER):
